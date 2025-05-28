d78371645518cddd606b1039d15af53126199752
cardosofede <federico.cardoso.e@gmail.com>
2025-05-28 17:24:04 +0200

    (feat) add logfire environment variable

--- config.py
+++ config.py
@@ -13,3 +13,4 @@ BROKER_USERNAME = os.getenv("BROKER_USERNAME", "admin")
 BROKER_PASSWORD = os.getenv("BROKER_PASSWORD", "password")
 PASSWORD_VERIFICATION_PATH = "bots/credentials/master_account/.password_verification"
 BANNED_TOKENS = os.getenv("BANNED_TOKENS", "NAV,ARS,ETHW,ETHF").split(",")
+LOGFIRE_ENVIRONMENT = os.getenv("LOGFIRE_ENVIRONMENT", "dev")
