2840ec1b8dc84d6976e05a90a8adfe902d8183fb
cardosofede <federico.cardoso.e@gmail.com>
2025-06-14 08:17:05 +0200

    (feat) move to repository model for orders and trades

--- database/__init__.py
+++ database/__init__.py
@@ -1,5 +1,7 @@
 from .models import AccountState, TokenState, Order, Trade, Base
 from .connection import AsyncDatabaseManager
 from .repositories import AccountRepository
+from .repositories.order_repository import OrderRepository
+from .repositories.trade_repository import TradeRepository
 
-__all__ = ["AccountState", "TokenState", "Order", "Trade", "Base", "AsyncDatabaseManager", "AccountRepository"]
+__all__ = ["AccountState", "TokenState", "Order", "Trade", "Base", "AsyncDatabaseManager", "AccountRepository", "OrderRepository", "TradeRepository"]
