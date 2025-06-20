f6720f8c2b6625456f839a8b15aeb726c71f5db0
cardosofede <federico.cardoso.e@gmail.com>
2025-06-20 17:37:59 +0200

    (feat) remove patches and let it fail

--- patches/__init__.py
+++
@@ -1,6 +0,0 @@
-"""
-Patches for third-party libraries used in the backend API
-"""
-from .config_helpers_patch import apply_config_helpers_patch, remove_config_helpers_patch
-
-__all__ = ['apply_config_helpers_patch', 'remove_config_helpers_patch']
