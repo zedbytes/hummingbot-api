22ab68cd19bb090702ded31558bfada79a906aca
cardosofede <federico.cardoso.e@gmail.com>
2025-06-30 20:50:50 +0200

    (feat) remove positions repository

--- database/__init__.py
+++ database/__init__.py
@@ -3,7 +3,6 @@ from .connection import AsyncDatabaseManager
 from .repositories import AccountRepository
 from .repositories.order_repository import OrderRepository
 from .repositories.trade_repository import TradeRepository
-from .repositories.position_repository import PositionRepository
 from .repositories.funding_repository import FundingRepository
 
-__all__ = ["AccountState", "TokenState", "Order", "Trade", "PositionSnapshot", "FundingPayment", "Base", "AsyncDatabaseManager", "AccountRepository", "OrderRepository", "TradeRepository", "PositionRepository", "FundingRepository"]
+__all__ = ["AccountState", "TokenState", "Order", "Trade", "PositionSnapshot", "FundingPayment", "Base", "AsyncDatabaseManager", "AccountRepository", "OrderRepository", "TradeRepository", "FundingRepository"]
